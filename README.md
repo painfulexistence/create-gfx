0404e6ceacf415e40dbd1c13707edd4fd9498c65
loic <910041@gmail.com>
2024-04-13 21:34:51 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Create GFX
+
+Set up a Web graphics application blazingly fast!
+
+## Usage
+
+```bash
+npx create-gfx <name>
+```
---
+++ src/cli.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import { create } from 'create-create-app';
+import { resolve } from 'path';
+
+const templateRoot = resolve(__dirname, '..', 'templates');
+
+const caveat = `
+This is a caveat!
+You can change this in \`src/cli.ts\`.
+`;
+
+create('create-gfx', {
+  templateRoot,
+  extra: {
+    architecture: {
+      type: 'list',
+      describe: 'choose your fave os',
+      choices: ['macOS', 'Windows', 'Linux'],
+      prompt: 'if-no-arg',
+    },
+  },
+  after: ({ answers }) => console.log(`Ok you chose ${answers.architecture}.`),
+  caveat,
+});
