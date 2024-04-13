2523bf7be3d4d1cd24fd646f1d8770c896898b3e
loic <910041@gmail.com>
2024-04-14 01:21:11 +0800

    Minor changes

--- src/cli.ts
+++ src/cli.ts
@@ -6,20 +6,10 @@ import { resolve } from 'path';
 const templateRoot = resolve(__dirname, '..', 'templates');
 
 const caveat = `
-This is a caveat!
-You can change this in \`src/cli.ts\`.
+Done!
 `;
 
 create('create-gfx', {
   templateRoot,
-  extra: {
-    architecture: {
-      type: 'list',
-      describe: 'choose your fave os',
-      choices: ['macOS', 'Windows', 'Linux'],
-      prompt: 'if-no-arg',
-    },
-  },
-  after: ({ answers }) => console.log(`Ok you chose ${answers.architecture}.`),
   caveat,
 });
