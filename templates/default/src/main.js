04907ada25def533dcb5f2cbf151f9a6e34698d9
loic <910041@gmail.com>
2024-04-16 00:33:55 +0800

    Update default template

---
+++ templates/default/src/main.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <canvas id="canvas" width="800" height="450" ></canvas>
+  </div>
+`
+const canvas = document.getElementById("canvas");
