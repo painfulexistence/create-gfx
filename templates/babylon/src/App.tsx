d5313cb4d9e45ee16354a1e9fb8bc26480df7e21
loic <910041@gmail.com>
2024-04-15 22:47:05 +0800

    feat: Add babylonjs template

---
+++ templates/babylon/src/App.tsx
@@ -0,0 +1,41 @@
+import { useRef, useEffect } from 'react'
+import { Engine } from '@babylonjs/core/Engines/engine';
+import { Scene } from '@babylonjs/core/scene';
+import { Vector3 } from '@babylonjs/core';
+import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { MeshBuilder } from "@babylonjs/core/Meshes";
+import './App.css';
+
+const App = () => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const engine = new Engine(canvas, true);
+
+      let scene = new Scene(engine);
+      let camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+      camera.setTarget(Vector3.Zero());
+      camera.attachControl(canvas, true);
+
+      let light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+      let box = MeshBuilder.CreateBox("box1", { size: 1 }, scene);
+
+      engine.runRenderLoop(() => {
+        scene.render();
+      });
+
+      window.addEventListener('resize', () => {
+        engine.resize();
+      });
+    }
+  }, []);
+
+  return (
+    <canvas ref={canvasRef} style={{ width: 800, height: 450 }}/>
+  )
+}
+
+export default App
