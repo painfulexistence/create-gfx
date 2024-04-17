cb726054f4ddab58a100461e64f678bd3dfb2735
loic <910041@gmail.com>
2024-04-17 16:20:00 +0800

    chore: update ThreeJS template

--- templates/three/src/App.tsx
+++ templates/three/src/App.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
 
-function App() {
-  return (
-    <>
-    </>
-  )
-}
+const App = () => {
+    return (
+        <Canvas style={{ height: 800, width: 450 }}>
+            <directionalLight position={[0, 5, 0]} />
+            <mesh rotation-x={Math.PI * 0.25} rotation-y={Math.PI * 0.25}>
+                <boxGeometry args={[2, 2, 2]} />
+                <meshStandardMaterial color={"white"} />
+            </mesh>
+        </Canvas>
+    );
+};
 
-export default App
+export default App;
