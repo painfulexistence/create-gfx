1bc36213128112d75cb54e4271f33d956c338cd5
loic <910041@gmail.com>
2024-04-20 03:25:23 +0800

    chore: update threejs template

--- templates/three/src/App.tsx
+++ templates/three/src/App.tsx
@@ -1,14 +1,17 @@
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 
 const App = () => {
     return (
-        <Canvas style={{ height: 800, width: 450 }}>
-            <directionalLight position={[0, 5, 0]} />
-            <mesh rotation-x={Math.PI * 0.25} rotation-y={Math.PI * 0.25}>
-                <boxGeometry args={[2, 2, 2]} />
-                <meshStandardMaterial color={"white"} />
-            </mesh>
+        <Canvas style={{ width: 800, height: 450 }}>
+            <Suspense fallback={null}>
+                <directionalLight position={[0, 5, 0]} />
+                <mesh rotation-x={Math.PI * 0.25} rotation-y={Math.PI * 0.25}>
+                    <boxGeometry args={[2, 2, 2]} />
+                    <meshStandardMaterial color="white" />
+                </mesh>
+            </Suspense>
         </Canvas>
     );
 };
